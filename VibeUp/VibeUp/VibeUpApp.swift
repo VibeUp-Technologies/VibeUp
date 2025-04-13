e3832bed13bd20420939d4d7afee4cbd8f03c24c
Nikita-Lizogubov-VibeUp <nikitalizogubovvibeup@gmail.com>
2025-04-13 20:21:38 +0300

    Init project

---
+++ VibeUp/VibeUp/VibeUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeUpApp.swift
+//  VibeUp
+//
+//  Created by Nikita Lizogubov on 13.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct VibeUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
